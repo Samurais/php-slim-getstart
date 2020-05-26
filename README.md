a32e56c8bc333ef4f486fd671770c37299f01f87
Hai Liang Wang <hain@chatopera.com>
2020-05-26 18:31:10 +0800

    Add loggger into container

---
+++ README.md
@@ -0,0 +1,13 @@
+# Slim Framework Get Start with PHP Coding
+
+```
+https://www.slimframework.com/docs/v3/tutorial/first-app.html
+```
+
+## Take away
+
+```
+composer install
+./admin/start.sh
+./admin/test.sh
+```
--- public/index.php
+++ public/index.php
@@ -3,13 +3,25 @@ require $_SERVER["DOCUMENT_ROOT"] . '/../vendor/autoload.php';
 
 // Create and configure Slim app
 $config = ['settings' => [
+    'displayErrorDetails' => true,
     'addContentLengthHeader' => false,
 ]];
 
-$app = new \Slim\App($config);
+$container = new \Slim\Container($config);
+
+// add logger
+$container['logger'] = function($c) {
+    $logger = new \Monolog\Logger('my_logger');
+    $file_handler = new \Monolog\Handler\StreamHandler('./logs/app.log',  \Monolog\Logger::INFO);
+    $logger->pushHandler($file_handler);
+    return $logger;
+};
+
+$app = new \Slim\App($container);
 
 // Define app routes
 $app->get('/hello/{name}', function ($request, $response, $args) {
+    $this->logger->info('Something interesting happened');
     return $response->write("Hello " . $args['name']);
 });
 
